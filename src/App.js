955d8db55285e6b1dde4a4893edf85d0d6b7356e
wdaitiw <idm00nbow@gmail.com>
2020-10-05 18:47:11 +0900

    learn about jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 
 function App() {
+  const dom = "Yo"
   return (
-    <div>Hellow world</div>
+    <React.Fragment>
+      <div>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => { console.log("daichi") }}></input>
+      </div>
+
+    </React.Fragment >
   );
 }
 
